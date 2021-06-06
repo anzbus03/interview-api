5d4ef56e7b1dee42d5c2a74deb4c502063c619af
Ahmed Nasser Zahra <73432971+anzbus03@users.noreply.github.com>
2021-06-06 14:16:08 +0400

    last

--- app/Http/Controllers/RoutesAll.php
+++ app/Http/Controllers/RoutesAll.php
@@ -40,17 +40,8 @@ class RoutesAll extends Controller
      */
     public function show($id)
     {
-        // $value2 = Transaction::find($id)->;
-        // // dd($value);where('id', '<=', $correctedComparisons)->count();
-        // $val::where('route_id', '==', $id)->count();
-        // echo($val);
         $user = DB::table('transactions')->where('route_id', $id)->get();
-        // echo($user);
         $route = Routes::find($id);
-        // print_r($value);
-        // die();
-        // echo '<pre>';
-        // print_r($value);
         foreach ((array)$user as $key => $valuet) {
             $arr = (array)$valuet;
             foreach ($arr as $key2 => $values) {
@@ -75,26 +66,9 @@ class RoutesAll extends Controller
                     ]);
                     echo json_encode($res->original, true);
                 }
-
             }
-                // $res = response()->json([
-                //     'data' => [
-                //         'salesman' => [
-                //             // 'route_id' => $key["id"],
-                //             'route_name' => $route['route_name'],
-                //             'route_no' => $route['route_no'],
-                //             'salesman_summary' => [
-                //                 // 'total_amount' => $key['total_amount'],
-                //                 'total_hour' => []
-                //             ]
-                //         ]
-                //     ]
-                // ]);
-                // return $res;
-            }
-
-
         }
+    }
 
 
 
